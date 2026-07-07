2d747175aae61e918704772e5272a470aa0528e2
Kamisenin <senelle.camille18@gmail.com>
2026-07-07 17:04:55 +0200

    ref: removed logging
    Signed-off-by: Kamisenin <senelle.camille18@gmail.com>

--- srcs/next/src/app/layout.tsx
+++ srcs/next/src/app/layout.tsx
@@ -21,7 +21,6 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
     const user = await getCurrentUser();
-    console.log(user);
     return (
         <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
         <body>
