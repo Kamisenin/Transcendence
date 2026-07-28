c65600ba0efc5dea1a63076774bb223a5583b6d2
ROLLAND <nathan.rolland066@gmail.com>
2026-07-28 21:26:31 +0200

    add files for 2fa with new page for the verif

--- srcs/next/src/app/api/auth/login/route.ts
+++ srcs/next/src/app/api/auth/login/route.ts
@@ -1,7 +1,11 @@
 import {createUser, getUser, isAccountIdUsed, isEmailUsed} from "%/lib/prisma-utils";
+import { sendVerifEmail, generateVerifCode, getVerifExpiry } from "@/app/lib/email";
 import {createSession, setCookies} from "%/lib/session";
-import { compare } from 'bcrypt';
 import { NextRequest, NextResponse } from 'next/server';
+import { compare } from 'bcrypt';
+import { cookies } from "next/headers";
+import { prisma } from "%/lib/prisma"
+import TwoFactorToggle from "@/components/TwoFactor";
 
 export async function POST(req: NextRequest) {
     const body = await req.json();
@@ -10,18 +14,32 @@ export async function POST(req: NextRequest) {
         return NextResponse.json({ error: "Field Required" }, { status: 400 });
 
     const user = await getUser(body.id);
-    if (!user) {
+    if (!user)
         return NextResponse.json({ error: "Invalid credentials" }, { status: 401 });
-    }
 
     const password = body.password as string;
     const passwordMatch = await compare(password, user.password);
-    if (!passwordMatch) {
+    if (!passwordMatch)
         throw new Error("Identifiants incorrects");
+    if (user.twoFactorEnabled) {
+        const code = generateVerifCode();
+        const expiry = getVerifExpiry();
+
+        await prisma.user.update({where: {user_id: user.user_id}, data: {verifCode: code, verifExpiry: expiry}});
+        await sendVerifEmail(user.email, code);
+
+        const cookieStore = await cookies();
+        cookieStore.set("pending_2fa_user", user.user_id, {
+            httpOnly: true,
+            sameSite: "lax",
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 60 * 10,
+        });
+        return NextResponse.json({success: true, TwoFactorRequired: true});
     }
 
     const stayConnected = body.stayConnected as boolean;
     const session = await createSession(user.user_id, stayConnected);
     await setCookies(session, stayConnected);
-    return NextResponse.json({ success: true, message: "User logged in" }, { status: 201 });
+    return NextResponse.json({ success: true, TwoFactorRequired: false});
 }
---
+++ srcs/next/src/app/verify_2fa/page.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function verify2faPage() {
+    const [code, setCode] = useState("");
+    const [message, setMessage] = useState("");
+    const [loading, setLoading] = useState(false);
+    const router = useRouter;
+
+    async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        setLoading(true);
+        setMessage("");
+
+        const res = await fetch("/api/auth/verify_2fa", {
+            method: "POST",
+            headers: {"Content-type": "application/json"},
+            body: JSON.stringify({code}),
+        });
+        const data = await res.json();
+
+        if (res.ok) {
+            window.location.href = "/";
+        } else {
+            setMessage(data.error || "Something went wrong");
+        }
+        setLoading(false);
+    }
+    return (
+        <div className="flex flex-col items-center justify-center mmin-h-screen">
+            <h1 className="text-2x1 font-bold mb-4">Two factor verification</h1>
+            <p className="text-sm text-gray-600 mb-4">We sent a code to your email.</p>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-3 w-80">
+                <input
+                    type="text"
+                    value={code}
+                    onChange={(e) => setCode(e.target.value)}
+                    maxLength={6}
+                    className="boder p-2 rounded text-black text-center tracking-widest"
+                    required
+                />
+                <button
+                    type="submit"
+                    disabled={loading}
+                    className="bg-blue-500 text-white p-2 rounded hover: bg-blue-600"
+                >
+                    {loading ? "Verifying..." : "Verify"}
+                </button>
+            </form>
+            {message && <p className="text-sm mt-4">{message}</p>}
+        </div>
+    );
+}
