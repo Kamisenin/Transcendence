e6672bfd16269cbb9cb7819e3edc76adae4cd252
Hugo <144733665+Diremii@users.noreply.github.com>
2026-08-03 14:25:11 +0200

    Add SEARCH_ENGINE_URL export to dev_start.sh

--- dev_start.sh
+++ dev_start.sh
@@ -3,5 +3,6 @@
 PSSWD=$(cat secrets/db_website_pwd.txt)
 
 export DATABASE_URL="postgres://website_db_admin:${PSSWD}@127.0.0.1:5432/WEBSITE?schema=public"
+export SEARCH_ENGINE_URL="http://localhost:8000"
 
 npm run dev --prefix srcs/next/
