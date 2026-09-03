50fd1c8c3c477bc903a777cdf492c4896bf3c6e4
Kamisenin <senelle.camille18@gmail.com>
2026-09-03 18:17:54 +0200

    fix : added the new gmail secrets to docker

--- srcs/next/entrypoint.sh
+++ srcs/next/entrypoint.sh
@@ -10,7 +10,9 @@ log "Initializing Database URL"
 
 DB_WEBSITE_PASSWD=$(cat /run/secrets/db_website_pwd)
 
-
 export DATABASE_URL="postgresql://website_db_admin:${DB_WEBSITE_PASSWD}@postgres:5432/WEBSITE?schema=public"
+export GMAIL_USER=$(cat /run/secrets/gmail_user)
+export GMAIL_APP_PASSWORD=$(cat /run/secrets/gmail_app_pwd)
+export SEARCH_ENGINE_URL="http://search-engine:8080"
 
 exec "$@"
