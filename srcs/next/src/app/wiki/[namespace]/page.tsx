2ef4eedd9449c2a23d957ca24fffd749c63e9701
Kamisenin <senelle.camille18@gmail.com>
2026-09-07 18:25:13 +0200

    feat : added page filtering for user, not tested

--- srcs/next/src/app/wiki/[namespace]/page.tsx
+++ srcs/next/src/app/wiki/[namespace]/page.tsx
@@ -1,44 +1,41 @@
 import ForumCard, { PageData } from "@/components/ForumCard";
-import { getOwnedPages } from "@/actions/pages";
+import { filterPages, getAccessiblePages, getEditablePages} from "@/actions/pages";
 import { getCurrentUser } from "%/lib/session";
+import { getUser } from "@/app/lib/prisma/prisma-utils";
+import { notFound } from "next/navigation";
 
 type Params = {
   params: Promise<{
-    username: string;
+    namespace: string;
   }>;
 };
 
 export default async function UserWikiPage({ params }: Params) {
-  const { username } = await params;
+  const { namespace } = await params;
 
- 
+  const target = await getUser(namespace);
+
+  if (!target)
+    notFound();
+
   const currentUser = await getCurrentUser();
 
-  const ownedPages = await getOwnedPages();
+  const ownedPages = await getEditablePages(namespace);
 
-
-  const formattedPages: PageData[] = ownedPages.map((p) => ({
-    pageId: p.pageId,
-    title: p.title,
-    description: null,
-    img: p.preview || null,
-    slug: p.canonicalSlug?.slug || "",
-    namespace: p.canonicalSlug?.namespace || "",
-    tags: [], 
-  }));
+  const filteredPages = await filterPages(currentUser?.user_id, ownedPages)
 
   return (
     <div className="min-h-screen container mx-auto p-6 pt-20">
       
       <section className="bg-card text-card-foreground border border-border rounded-lg p-4 shadow-md">
         <h2 className="text-xl font-bold mb-4">
-          Pages créées par {username} ({formattedPages.length})
+          Pages créées par {namespace} ({filteredPages.length})
         </h2>
 
         <div className="max-h-[600px] overflow-y-auto pr-2 space-y-4 scrollbar-thin">
-          {formattedPages.length > 0 ? (
+          {filteredPages.length > 0 ? (
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              {formattedPages.map((page) => (
+              {filteredPages.map((page) => (
                 <ForumCard
                   key={page.pageId}
                   page={page}
