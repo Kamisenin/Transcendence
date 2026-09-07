765fb1cc087d5a87ecca4a31523157f28b3d8bce
Kamisenin <senelle.camille18@gmail.com>
2026-09-07 14:44:48 +0200

    ref : moved Two FactorToggle to account

--- srcs/next/src/app/account/page.tsx
+++ srcs/next/src/app/account/page.tsx
@@ -2,11 +2,13 @@ import { getCurrentUser } from "%/lib/session";
 import { redirect } from "next/navigation";
 import AccountForm from "@/components/AccountForm";
 import { getTranslations } from "next-intl/server";
+import TwoFactorToggle from "@/components/TwoFactor"
 import DeleteAccountButton from "@/components/DeleteAccountButton";
 import SessionList from "@/components/SessionsList";
 
 export default async function AccountPage() {
     const user = await getCurrentUser();
+    const t = await getTranslations("Account");
 
     if (!user) {
         redirect("/login");
@@ -15,10 +17,11 @@ export default async function AccountPage() {
         <main className="pt-20 flex justify-center">
             <div className="w-96 bg-white p-6 rounded shadow">
                 <h1 className="text-2xl font-bold mb-4">
-                    My Account
+                    {t("title")}
                 </h1>
                 <AccountForm user={user}/>
-                <div className="border-t pt-4">
+                <TwoFactorToggle twoFactorEnabled={user.twoFactorEnabled} emailVerified={user.emailVerified}/>
+                <div className="pt-8">
                     <SessionList/>
                 </div>
                 <div className="border-t pt-4">
